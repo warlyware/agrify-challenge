1bfe2d94a0a4bb9b686e18737a3cb1007aa618e3
Dan Ward <danward@gmail.com>
2020-07-18 11:08:09 -0700

    Add working strategy for updating all values when input changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(constant, key) in constants" :key="key">
+      {{fields[key].label}}<br>
+      <input @input="update($event, key)"
+      :value="calculate(key)"
+      v-if="!fields[key].readOnly">
+      <div v-else>{{calculate(key)}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import constants from './constants/constants.json'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    let mappedConstants = {}
+    for (const [key, value] of Object.entries(constants.fields)) {
+      if (value.defaultValue >= 0) {
+        mappedConstants[key] = value.defaultValue
+      } else {
+        mappedConstants[key] = value.calculate
+      }
+    }
+    return {
+      ...mappedConstants,
+      constants: mappedConstants,
+      fields: constants.fields
+    }
+  },
+  mounted() {
+    for (const [key, value] of Object.entries(constants.fields)) {
+      if (value.calculate) {
+        this[key] = eval(value.calculate)
+        this.constants[key] = eval(value.calculate)
+      }
+    }
+  },
+  methods: {
+    update(ev, key) {
+      this[key] = ev.target.value
+      this.constants[key] = ev.target.value
+      this.$forceUpdate()
+    },
+    calculate(key) {
+      return this.fields[key].calculate ? eval(this.fields[key].calculate) : this[key]
+    }
   }
 }
 </script>
